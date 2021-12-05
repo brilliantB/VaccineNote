a5478097a4399f976996ae566851f0ba4fe56732
B <89058210+brilliantB@users.noreply.github.com>
2021-12-05 21:14:55 +0900

    Delete routes directory

--- routes/vaccine/index.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var authRouter = require("./auth/index");
-var postRouter = require("./post/index");
-
-router.use("/auth", authRouter);
-router.use("/post", postRouter);
-module.exports = router;
